e1eb7d8903e6c2f62ef22ccb6eaec2d6a501e648
maroctamorg <guimaraesneto.marcelo@gmail.com>
2021-11-21 12:01:49 +0200

    work on escaped characters in parser

--- src/game/parser.cpp
+++ src/game/parser.cpp
@@ -1,5 +1,55 @@
 #include "parser.hpp"
 
+bool Parser::getStringUpToChar(std::stringstream& stream, std::string& sentence, char delim) {
+    std::string buffer;
+    char previous {NULL};
+    sentence.clear();
+    do {
+        if(previous) {
+            sentence += delim;
+            stream.seekg(stream.tellg() + 1);
+        }
+        std::getline(stream, buffer, delim);
+        sentence.append(buffer);
+        stream.seekg(stream.tellg() - 2);
+        stream >> previous;
+    } while(previous == '\\');
+
+    return !sentence.empty();
+}
+
+void Parser::parseEscapedChar(std::string& sentence) {
+    std::stringstream stream {sentence};
+    std::string buffer;
+    char next;
+    sentence.clear();
+    while (std::getline(stream, buffer, '\\')) {
+        sentence.append(buffer);
+        stream >> next;
+        switch(next) {
+            case('n'): {
+                sentence += '\n';
+                break;
+            }
+            case('\\'): {
+                sentence += '\\';
+                stream.seekg(stream.tellg() + 1);
+            }
+            default: {
+                break;
+            }
+        }
+    }
+}
+
+const std::map<char, uint> Parser::alphabet {
+    {' ', 0},   {'a', 1},   {'b', 2},   {'c', 3},   {'d', 4},   {'e', 5},
+    {'f', 6},   {'g', 7},   {'h', 8},   {'i', 9},   {'j', 10},  {'k', 11},
+    {'l', 12},  {'m', 13},  {'n', 14},  {'o', 15},  {'p', 16},  {'q', 17},
+    {'r', 18},  {'s', 19},  {'t', 20},  {'u', 21},  {'v', 22},  {'w', 23},
+    {'x', 24},  {'y', 25},  {'z', 26},  {'-', 27},  {',', 28},  {'.', 29}
+};
+
 long Parser::uniqueId(std::string word) {
     std::stringstream number_s;
     for(auto it = word.cbegin(); it < word.cend(); it++) {
@@ -55,8 +105,9 @@ bool Parser::execute(std::stringstream sentence) {
         }
         case SAY: {
             std::string content;
-            std::getline(sentence, buffer, '\'');
-            std::getline(sentence, content, '\'');
+            getStringUpToChar(sentence, buffer, '\'');
+            getStringUpToChar(sentence, content, '\'');
+            parseEscapedChar(content);
             game.write(content);
             return true;
             break;
@@ -64,17 +115,18 @@ bool Parser::execute(std::stringstream sentence) {
         case RES: {
             std::vector<std::string> options;
             std::string option;
-            std::getline(sentence, buffer, '\'');
+            getStringUpToChar(sentence, buffer, '\'');
             std::cout << "Entering loop...\n";
-            while(std::getline(sentence, option, '\'')) {
+            while(getStringUpToChar(sentence, option, '\'')) {
                 if(option.empty() || option == "f this?") break;
                 std::cout << "option:" << option << '\n';
                 std::cout << "sentence position: " << sentence.tellg() << '\n';
                 options.push_back(option);
-                std::getline(sentence, buffer, '\'');
+                getStringUpToChar(sentence, buffer, '\'');
                 option.clear();
             }
             std::cout << "Exiting loop...\n";
+            std::for_each(options.begin(), options.end(), parseEscapedChar); // what is passed to the function: the iterator or the content it is pointing to?
             game.writeAnswers(options);
             return true;
             break;
@@ -85,7 +137,8 @@ bool Parser::execute(std::stringstream sentence) {
             if(selection == argument) {
                 selection = 0;
                 std::string subsentence;
-                std::getline(sentence, subsentence);
+                // sentence.seekg(sentence.tellg()+1); // not needed, the stream >> already ignores whitespaces + is quite ugly and inflexible
+                getStringUpToChar(sentence, subsentence, '.');
                 this->execute(std::stringstream(argument + " " + subsentence));
             }
             return true;
@@ -102,8 +155,8 @@ bool Parser::execute(std::stringstream sentence) {
         }
         case LOAD: {
             std::string scene;
-            std::getline(sentence, buffer, '\'');
-            std::getline(sentence, scene, '\'');
+            getStringUpToChar(sentence, buffer, '\'');
+            getStringUpToChar(sentence, scene, '\'');
             loadScene(scene);
             return true;
             break;
@@ -118,10 +171,11 @@ bool Parser::execute(std::stringstream sentence) {
 bool Parser::execute() {
     // std::cout << "Call to parser execute\n";
     std::string sentence;
-    std::getline(script, sentence);
+    getStringUpToChar(script, sentence, '.');
     // std::cout << "\nScript: " << script.str() << "\n\n";
     // std::cout << "Sentence being parsed: " << sentence << '\n';
     this->execute(std::stringstream(sentence));
+    std::getline(script, sentence); //flushes the script stream to the next line
     // std::cout << "Exiting parser execute\n";
 }
 
--- src/game/parser.hpp
+++ src/game/parser.hpp
@@ -9,40 +9,12 @@ private:
 
 
 private:
-    const std::map<char, uint> alphabet {
-        {' ', 0},
-        {'a', 1},
-        {'b', 2},
-        {'c', 3},
-        {'d', 4},
-        {'e', 5},
-        {'f', 6},
-        {'g', 7},
-        {'h', 8},
-        {'i', 9},
-        {'j', 10},
-        {'k', 11},
-        {'l', 12},
-        {'m', 13},
-        {'n', 14},
-        {'o', 15},
-        {'p', 16},
-        {'q', 17},
-        {'r', 18},
-        {'s', 19},
-        {'t', 20},
-        {'u', 21},
-        {'v', 22},
-        {'w', 23},
-        {'x', 24},
-        {'y', 25},
-        {'z', 26},
-        {'-', 27},
-        {',', 28},
-        {'.', 29}
-    };
+    static bool getStringUpToChar(std::stringstream& stream, std::string& sentence, char delim);
+    static void parseEscapedChar(std::string& sentence);
 
-    long uniqueId(std::string word);
+    static const std::map<char, uint> alphabet;
+
+    static long uniqueId(std::string word);
 
     enum verb {
         SAY  = 19125,
